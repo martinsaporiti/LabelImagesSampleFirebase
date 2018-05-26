ffcdaf30fa54129d12b1d62c8dc9530c880e2985
Martín Saporiti <martinsaporiti@gmail.com>
2018-05-26 10:05:53 -0300

    Cambios necesarios para poder seleccionar cloud/local

--- LabelImagesSampleFirebase/ViewController.swift
+++ LabelImagesSampleFirebase/ViewController.swift
@@ -18,14 +18,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     lazy var vision = Vision.vision()
     
     var imagePicker : UIImagePickerController? = UIImagePickerController()
-
+    
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    @IBOutlet weak var switchCloudOption: UISwitch!
     
     // [START config_label]
-//    let options = VisionLabelDetectorOptions(confidenceThreshold: 0.7)
+    let options = VisionLabelDetectorOptions(confidenceThreshold: 0.7)
     
-    let options : VisionCloudDetectorOptions = {
+    let cloudOptions : VisionCloudDetectorOptions = {
         let options = VisionCloudDetectorOptions()
         options.modelType = .latest
         options.maxResults = 20
@@ -43,7 +44,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker?.delegate = self;
         
         activityIndicator.hidesWhenStopped = true;
-        activityIndicator.center = view.center;
+            activityIndicator.center = view.center;
 
     }
 
@@ -53,40 +54,75 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     
-    func loadLabels(imageToEvaluate: UIImage){
-
+    func loadLabelsWithCloud(imageToEvaluate: UIImage){
+        let labelDetector = Vision.vision().cloudLabelDetector(options: cloudOptions)
+        let visionImage = VisionImage(image: imageToEvaluate)
         self.result.text = "";
 
-        self.imageView.image = imageToEvaluate
-//        let labelDetector = vision.labelDetector(options: self.options)
-        let labelDetector = Vision.vision().cloudLabelDetector(options: options)
-        let visionImage = VisionImage(image: imageToEvaluate)
-        
-        
         self.activityIndicator.startAnimating()
-        DispatchQueue.main.async {
+        
             labelDetector.detect(in: visionImage) { (labels, error) in
                 guard error == nil, let labels = labels, !labels.isEmpty else {
+                    self.result.text = "Error!";
+                    self.activityIndicator.stopAnimating()
                     return
                 }
                 
-
                 var labelText : String? = ""
                 var confidence: NSNumber? = 0
                 var entityToPrint : String? = ""
                 for label in labels {
                     labelText = label.label!
-    //                var entityId = label.entityID
+                    //                var entityId = label.entityID
                     confidence = label.confidence
+                    
+                    confidence = NSNumber(value: round((confidence?.doubleValue)! * 100))
+                    entityToPrint = " \(labelText!) - \(confidence!)% \n"
+                    self.result.text.append(entityToPrint!)
+                }
+                self.activityIndicator.stopAnimating()
+                
+            }
+        
+    }
+    
+    func loadLabels(imageToEvaluate: UIImage){
 
+        self.result.text = "";
+
+        self.imageView.image = imageToEvaluate
+        
+        var labelDetector : VisionLabelDetector
+        if(self.switchCloudOption.isOn){
+            self.loadLabelsWithCloud(imageToEvaluate: imageToEvaluate)
+            return
+        } else {
+            labelDetector = vision.labelDetector(options: self.options)
+        }
+        
+        let visionImage = VisionImage(image: imageToEvaluate)
+        
+        self.activityIndicator.startAnimating()
+      
+            labelDetector.detect(in: visionImage) { (labels, error) in
+                guard error == nil, let labels = labels, !labels.isEmpty else {
+                    return
+                }
+                
+                var labelText : String? = ""
+                var confidence: NSNumber? = 0
+                var entityToPrint : String? = ""
+                for label in labels {
+                    labelText = label.label
+//                    var entityId = label.entityID
+                    confidence = label.confidence as NSNumber
                     confidence = NSNumber(value: round((confidence?.doubleValue)! * 100))
                     entityToPrint = " \(labelText!) - \(confidence!)% \n"
                     self.result.text.append(entityToPrint!)
                 }
                 self.activityIndicator.stopAnimating()
             }
-            
-        }
+    
     }
     
     
@@ -112,6 +148,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker!.allowsEditing = false
         imagePicker!.sourceType = UIImagePickerControllerSourceType.photoLibrary
         self.present(imagePicker!, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func changeCloudOption(_ sender: Any) {
+        self.loadLabels(imageToEvaluate: self.imageView.image!)
     }
     
 }
